3b796dc75b92b068bb832f27f3b47862e1f07fa0
goinsa-cmd <goinsa0854@student.faytechcc.edu>
2025-09-07 22:48:51 +0000

    done

---
+++ M1T1: Hello World.cpp
@@ -0,0 +1,16 @@
+/*
+ M1T1: Hello World Assignement 
+ Date: september 7, 2025
+*/
+
+#include <iostream> 
+using namespace std;
+
+
+int main()
+{
+    //displays Hello world in the console
+    cont << "Hello World!" << endl;
+
+    return 0;
+}
