6b26458004a0a44cbf9aebcdd30667384f110622
goinsa-cmd <goinsa0854@student.faytechcc.edu>
2025-09-13 23:44:04 +0000

    Hopefully it works

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -1,14 +1,27 @@
 /*
- M1LAB: Apple Sales 
+ CSC 134 
+ M1LAB: Apple Sales
+ Aaron Goins 
  Date: september 13, 2025
 */
 
 #include <iostream> 
 using namespace std;
 
-
 int main()
 {
+    // The cost to buy bananas per pound
+    string item_name = "bananas";
+    string shop_name = "The banana tree";
+    double price_of_item = .75;
+    int number_of_items;
+    int items_bought;
+
+    // TODO get user input
+    cout << "Welcome to " << shop_name << " store!" << endl; 
+    cout << "We currently have " << number_of_items << " " << item_name << endl;
+
+
 
 
     return 0;
