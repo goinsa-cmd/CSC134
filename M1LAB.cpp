bef36e104dde7d0dbdf37fed07da6a974614c357
goinsa-cmd <goinsa0854@student.faytechcc.edu>
2025-09-13 23:44:29 +0000

    test

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -11,15 +11,16 @@ using namespace std;
 int main()
 {
     // The cost to buy bananas per pound
-    string item_name = "bananas";
+    string item_name = "banana";
     string shop_name = "The banana tree";
     double price_of_item = .75;
-    int number_of_items;
+    int number_of_items = 100;
     int items_bought;
 
-    // TODO get user input
     cout << "Welcome to " << shop_name << " store!" << endl; 
-    cout << "We currently have " << number_of_items << " " << item_name << endl;
+    cout << "We currently have " << number_of_items << " " << item_name << "s for sale" << endl;
+    cout << "The price of one " << item_name << " is " << price_of_item << "$" << endl;
+    cout << "if you want them all, that will cost you " << price_of_item*number_of_items << "$" << endl << endl;
 
 
 
