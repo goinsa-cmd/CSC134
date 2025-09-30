fdec258c48a477ee643f3afa490678cdc47098a7
goinsa-cmd <goinsa0854@student.faytechcc.edu>
2025-09-30 22:28:53 +0000

    Finish

---  M2T2.cpp
+++  M2T2.cpp
@@ -20,6 +20,7 @@ int main()
     //variables
      string store_name = "plenters";
      string item = "plenter";
+     string empty = "               ";
      double item_price = 2.17 ; 
      double sales_tax = .19 ;
      double tax_total;
@@ -27,16 +28,21 @@ int main()
 
     //Greet
     cout << "Welcome to " << store_name  << endl;
-    cout << "you have orderd one " + item << endl;
-
-
+    cout << "you have ordered one " + item << endl;
+    cout << "-------------------------------------" << endl;
     //Calculate
 
     tax_total = item_price*sales_tax;
     total_price = item_price + tax_total;
 
-    cout << setprecision << fixed;
-    cout << total_price << endl << endl;
+
+    //Print
+    cout << item <<  empty.substr(0,empty.length()-item.length()) << item_price << endl;
+    cout << "Tax" <<empty.substr(0,empty.length()-3)  << tax_total  << endl;
+    cout << "-------------------------------------" << endl;
+    cout << setprecision(2) << fixed;
+    cout << "total" << empty.substr(0,empty.length()-5)  << total_price << endl;
+    cout << "Have a nice day!" << endl;
     
 
  
