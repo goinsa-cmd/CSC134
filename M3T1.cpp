fe3b8328210a433fb4c2725698e29a8ecbbcb226
goinsa-cmd <goinsa0854@student.faytechcc.edu>
2025-10-10 20:01:00 -0400

    Add files via upload

---
+++ M3T1.cpp
@@ -0,0 +1,46 @@
+// CSC 134
+// M3T1 
+// Aaron Goins
+// 10/8/2025
+// Ask the user for the width and length of two rectangles
+// Find the area
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    // Variables
+    double len1, wid1, len2, wid2; 
+    double area1, area2;
+
+    // Ask user for the length and width
+    cout << "What's the length of the first rectangle? ";
+    cin >> len1;
+    cout << "What's the width of the first rectangle? ";
+    cin >> wid1;
+    cout << "What's the length of the second rectangle? ";
+    cin >> len2;
+    cout << "What's the width of the second rectangle? ";
+    cin >> wid2;
+
+    // Find the area
+    area1 = len1 * wid1 ;
+    area2 = len2 * wid2 ;
+
+    // Print the area
+    cout << "Area one is :" << area1 << endl;
+    cout << "Area two is :" << area2 << endl;
+    if (area1 > area2){
+        cout << "Area one is bigger" << endl;
+    }
+       if (area2 > area1){
+        cout << "Area two is bigger" << endl;
+    }
+       if (area1 == area2){
+        cout << "The area of the rectangles are equal" << endl;
+    }
+
+    return 0;
+}
